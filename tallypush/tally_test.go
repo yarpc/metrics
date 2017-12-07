1592c7c16bd1e78c671f8dbc6289a511779e58d7
Akshay Shah <shah@uber.com>
2017-12-07 13:45:19 -0800

    Add a Tally-backed implementation of push.Target
    Add a `tallypush` package with a Tally-based implementation of the
    `push.Target` interface. Keeping the concrete implementation in a
    separate package from the interfaces ensures that only applications that
    actually push to Tally have to compile the Tally code.
    
    This commit includes simple unit tests for the Tally pusher types. A
    more thorough integration test will be included with the commit that
    integrates these push subpackages with the main `metrics.Controller`
    struct.

---
+++ tallypush/tally_test.go
@@ -0,0 +1,83 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package tallypush
+
+import (
+	"math"
+	"testing"
+
+	"go.uber.org/net/metrics"
+	"go.uber.org/net/metrics/push"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/uber-go/tally"
+)
+
+func newScope() tally.TestScope {
+	return tally.NewTestScope("" /* prefix */, nil /* tags */)
+}
+
+func TestCounter(t *testing.T) {
+	scope := newScope()
+	target := New(scope)
+	c := target.NewCounter(push.Opts{
+		Name:   "test_counter",
+		Labels: metrics.Labels{"foo": "bar"},
+	})
+	c.Set(10)
+	c.Set(20) // should overwrite previous value
+	counters := scope.Snapshot().Counters()
+	require.Equal(t, 1, len(counters), "Unexpected number of counters.")
+	assert.Equal(t, int64(20), counters["test_counter+foo=bar"].Value(), "Unexpected exported value.")
+}
+
+func TestGauge(t *testing.T) {
+	scope := newScope()
+	target := New(scope)
+	g := target.NewGauge(push.Opts{
+		Name:   "test_gauge",
+		Labels: metrics.Labels{"foo": "bar"},
+	})
+	g.Set(10)
+	g.Set(20) // should overwrite previous value
+	gauges := scope.Snapshot().Gauges()
+	require.Equal(t, 1, len(gauges), "Unexpected number of gauges.")
+	assert.Equal(t, 20.0, gauges["test_gauge+foo=bar"].Value(), "Unexpected exported value.")
+}
+
+func TestHistogram(t *testing.T) {
+	scope := newScope()
+	target := New(scope)
+	h := target.NewHistogram(push.HistogramOpts{
+		Opts:    push.Opts{Name: "test_histogram", Labels: metrics.Labels{"foo": "bar"}},
+		Buckets: []int64{5, 10, math.MaxInt64},
+	})
+	h.Set(5, 1)
+	h.Set(5, 2) // should overwrite previous value
+	histograms := scope.Snapshot().Histograms()
+	require.Equal(t, 1, len(histograms), "Unexpected number of histograms.")
+	assert.Equal(
+		t,
+		map[float64]int64{5: 2, 10: 0, math.MaxFloat64: 0},
+		histograms["test_histogram+foo=bar"].Values(),
+	)
+}
