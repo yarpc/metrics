6c895cd79d2f91b0e9072a00708d8d4eb87dd8e3
Akshay Shah <shah@uber.com>
2017-12-08 22:10:16 -0800

    Add some package-level documentation
    Add a package-level comment, a top-level example, and a number of
    type-specific examples.

---
+++ example_test.go
@@ -0,0 +1,256 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package metrics_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"time"
+
+	"github.com/uber-go/tally"
+	"go.uber.org/net/metrics"
+	"go.uber.org/net/metrics/tallypush"
+)
+
+func Example() {
+	root := metrics.New()
+	scope := root.Scope().Tagged(metrics.Tags{
+		"host":   "db01",
+		"region": "us-west",
+	})
+
+	total, err := scope.Counter(metrics.Spec{
+		Name: "selects_completed",
+		Help: "Total number of completed SELECT queries.",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	progress, err := scope.GaugeVector(metrics.Spec{
+		Name:    "selects_in_progress",
+		Help:    "Number of in-progress SELECT queries.",
+		VarTags: []string{"table"},
+	})
+	if err != nil {
+		panic(err)
+	}
+	trips := progress.MustGet("table" /* tag name */, "trips" /* tag value */)
+	drivers := progress.MustGet("table", "drivers")
+
+	fmt.Println("Trips:", trips.Inc())
+	total.Inc()
+	fmt.Println("Drivers:", drivers.Add(2))
+	total.Add(2)
+	fmt.Println("Drivers:", drivers.Dec())
+	fmt.Println("Trips:", trips.Dec())
+	fmt.Println("Total:", total.Load())
+
+	// Output:
+	// Trips: 1
+	// Drivers: 2
+	// Drivers: 1
+	// Trips: 0
+	// Total: 3
+}
+
+func ExampleCounter() {
+	c, err := metrics.New().Scope().Counter(metrics.Spec{
+		Name:      "selects_completed",                         // required
+		Help:      "Total number of completed SELECT queries.", // required
+		ConstTags: metrics.Tags{"host": "db01"},                // optional
+	})
+	if err != nil {
+		panic(err)
+	}
+	c.Add(2)
+}
+
+func ExampleCounterVector() {
+	vec, err := metrics.New().Scope().CounterVector(metrics.Spec{
+		Name:      "selects_completed_by_table",                   // required
+		Help:      "Number of completed SELECT queries by table.", // required
+		ConstTags: metrics.Tags{"host": "db01"},                   // optional
+		VarTags:   []string{"table"},                              // required
+	})
+	if err != nil {
+		panic(err)
+	}
+	vec.MustGet("table" /* tag name */, "trips" /* tag value */).Inc()
+}
+
+func ExampleGauge() {
+	g, err := metrics.New().Scope().Gauge(metrics.Spec{
+		Name:      "selects_in_progress",                       // required
+		Help:      "Total number of in-flight SELECT queries.", // required
+		ConstTags: metrics.Tags{"host": "db01"},                // optional
+	})
+	if err != nil {
+		panic(err)
+	}
+	g.Store(11)
+}
+
+func ExampleGaugeVector() {
+	vec, err := metrics.New().Scope().GaugeVector(metrics.Spec{
+		Name:      "selects_in_progress_by_table",                 // required
+		Help:      "Number of in-flight SELECT queries by table.", // required
+		ConstTags: metrics.Tags{"host": "db01"},                   // optional
+		VarTags:   []string{"table"},                              // optional
+	})
+	if err != nil {
+		panic(err)
+	}
+	vec.MustGet("table" /* tag name */, "trips" /* tag value */).Store(11)
+}
+
+func ExampleHistogram() {
+	h, err := metrics.New().Scope().Histogram(metrics.HistogramSpec{
+		Spec: metrics.Spec{
+			Name:      "selects_latency_ms",         // required, should indicate unit
+			Help:      "SELECT query latency.",      // required
+			ConstTags: metrics.Tags{"host": "db01"}, // optional
+		},
+		Unit:    time.Millisecond,                      // required
+		Buckets: []int64{5, 10, 25, 50, 100, 200, 500}, // required
+	})
+	if err != nil {
+		panic(err)
+	}
+	h.Observe(37 * time.Millisecond)
+}
+
+func ExampleHistogramVector() {
+	vec, err := metrics.New().Scope().HistogramVector(metrics.HistogramSpec{
+		Spec: metrics.Spec{
+			Name:      "selects_latency_by_table_ms",    // required, should indicate unit
+			Help:      "SELECT query latency by table.", // required
+			ConstTags: metrics.Tags{"host": "db01"},     // optional
+			VarTags:   []string{"table"},
+		},
+		Unit:    time.Millisecond,                      // required
+		Buckets: []int64{5, 10, 25, 50, 100, 200, 500}, // required
+	})
+	if err != nil {
+		panic(err)
+	}
+	vec.MustGet("table" /* tag name */, "trips" /* tag value */).Observe(37 * time.Millisecond)
+}
+
+func ExampleRoot_ServeHTTP() {
+	// First, construct a root and add some metrics.
+	root := metrics.New()
+	c, err := root.Scope().Counter(metrics.Spec{
+		Name:      "example",
+		Help:      "Counter demonstrating HTTP exposition.",
+		ConstTags: metrics.Tags{"host": "example01"},
+	})
+	if err != nil {
+		panic(err)
+	}
+	c.Inc()
+
+	// Expose the root on your HTTP server of choice.
+	mux := http.NewServeMux()
+	mux.Handle("/debug/net/metrics", root)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	// Your metrics are now exposed via a Prometheus-compatible handler. This
+	// example shows text output, but clients can also request the protocol
+	// buffer binary format.
+	res, err := http.Get(fmt.Sprintf("%v/debug/net/metrics", srv.URL))
+	if err != nil {
+		panic(err)
+	}
+	text, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(text))
+
+	// Output:
+	// # HELP example Counter demonstrating HTTP exposition.
+	// # TYPE example counter
+	// example{host="example01"} 1
+}
+
+func ExampleRoot_Push() {
+	// First, we need something to push to. In this example, we'll use Tally's
+	// testing scope.
+	ts := tally.NewTestScope("" /* prefix */, nil /* tags */)
+	root := metrics.New()
+
+	// Push updates to our test scope twice per second.
+	stop, err := root.Push(tallypush.New(ts), 500*time.Millisecond)
+	if err != nil {
+		panic(err)
+	}
+	defer stop()
+
+	c, err := root.Scope().Counter(metrics.Spec{
+		Name: "example",
+		Help: "Counter demonstrating push integration.",
+	})
+	if err != nil {
+		panic(err)
+	}
+	c.Inc()
+
+	// Sleep to make sure that we run at least one push, then print the counter
+	// value as seen by Tally.
+	time.Sleep(2 * time.Second)
+	fmt.Println(ts.Snapshot().Counters()["example+"].Value())
+
+	// Output:
+	// 1
+}
+
+func ExampleRoot_Snapshot() {
+	// Snapshots are the simplest way to unit test your metrics. A future
+	// release will add a more full-featured metricstest package.
+	root := metrics.New()
+	c, err := root.Scope().Counter(metrics.Spec{
+		Name:      "example",
+		Help:      "Counter demonstrating snapshots.",
+		ConstTags: metrics.Tags{"foo": "bar"},
+	})
+	if err != nil {
+		panic(err)
+	}
+	c.Inc()
+
+	// It's safe to snapshot your metrics in production, but keep in mind that
+	// taking a snapshot is relatively slow and expensive.
+	actual := root.Snapshot().Counters[0]
+	expected := metrics.Snapshot{
+		Name:  "example",
+		Value: 1,
+		Tags:  metrics.Tags{"foo": "bar"},
+	}
+	if !reflect.DeepEqual(expected, actual) {
+		panic(fmt.Sprintf("expected %v, got %v", expected, actual))
+	}
+}
